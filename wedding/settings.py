80bfc1d271e127908e53efdd8339fb91b81d4859
Vlad <blackbird.vlad@gmail.com>
2021-05-18 20:47:19 +0300

    add allowed hosts

--- wedding/settings.py
+++ wedding/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,11 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-pt^*=a$wqchy8gwjv76r8le2x&hi60+@eb9a1*^k--w%b7h&ul'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+# SECURITY WARNING: don't run with debug turned on in production
 # Application definition
 
 INSTALLED_APPS = [
@@ -123,3 +119,14 @@ STATIC_ROOT = BASE_DIR / "static_prod"
 STATICFILES_DIRS = [
     "static"
 ]
+
+if os.environ.get('PROD'):
+    try:
+        from .settings_prod import *
+    except ImportError:
+        pass
+else:
+    try:
+        from .settings_local import *
+    except ImportError:
+        pass
