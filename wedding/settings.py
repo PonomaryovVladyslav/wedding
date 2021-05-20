36f58640cb64390cde17a7edcafb8638a5143d20
Vlad <blackbird.vlad@gmail.com>
2021-05-20 13:42:52 +0300

    try to fix some bugs)

--- wedding/settings.py
+++ wedding/settings.py
@@ -121,24 +121,6 @@ STATICFILES_DIRS = [
     "static"
 ]
 
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl': 'max-age=94608000',
-}
-# Required
-AWS_STORAGE_BUCKET_NAME = 'wedding-vlad-katya'
-AWS_S3_REGION_NAME = 'us-east-1'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = os.environ.get('key')
-AWS_SECRET_ACCESS_KEY = os.environ.get('secret')
-
-# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
-# you run `collectstatic`).
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'wedding.custom_storages.StaticStorage'
-
-MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'wedding.custom_storages.MediaStorage'
-
 if os.environ.get('PROD'):
     try:
         from .settings_prod import *
