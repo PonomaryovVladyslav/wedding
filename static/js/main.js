e72b9a6c3da184c5e93019501582d4cc621b45ce
Vlad <blackbird.vlad@gmail.com>
2021-05-20 18:06:08 +0300

    add scroll top button

--- static/js/main.js
+++ static/js/main.js
@@ -305,6 +305,29 @@
     };
     let owl = $(".owl-carousel")
     owl.owlCarousel(owl_settings);
+
+    let mybutton = document.getElementById("scrollTop");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    $("#scrollTop").click(function (event) {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+
+
     // Document on load.
     $(function () {
 
